c184a4c9fca3665e671954bb45dc1f95c26b6e37
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-04-10 00:34:55 +0200

    set.Iter() method

--- set/set.go
+++ set/set.go
@@ -39,7 +39,7 @@ func (s *Set[E]) Has(v E) bool {
 func (s *Set[E]) Intersection(other *Set[E]) *Set[E] {
 	empty := slices.Values([]E{})
 	intersected := New(empty)
-	for item := range s.items {
+	for item := range s.Iter() {
 		if other.Has(item) {
 			intersected.Put(item)
 		}
@@ -50,7 +50,7 @@ func (s *Set[E]) Intersection(other *Set[E]) *Set[E] {
 func (s *Set[E]) Difference(other *Set[E]) *Set[E] {
 	empty := slices.Values([]E{})
 	difference := New(empty)
-	for item := range s.items {
+	for item := range s.Iter() {
 		if !other.Has(item) {
 			difference.Put(item)
 		}
@@ -67,7 +67,7 @@ func (s *Set[E]) Equals(other *Set[E]) bool {
 
 func (s *Set[E]) Union(other *Set[E]) *Set[E] {
 	unioned := New(s.Items())
-	for item := range other.items {
+	for item := range other.Iter() {
 		unioned.Put(item)
 	}
 	return unioned
@@ -77,12 +77,12 @@ func (s *Set[E]) SymmetricDifference(other *Set[E]) *Set[E] {
 	empty := slices.Values([]E{})
 	diff1 := New(empty)
 	diff2 := New(empty)
-	for item := range s.items {
+	for item := range s.Iter() {
 		if !other.Has(item) {
 			diff1.Put(item)
 		}
 	}
-	for item := range other.items {
+	for item := range other.Iter() {
 		if !s.Has(item) {
 			diff2.Put(item)
 		}
@@ -91,7 +91,7 @@ func (s *Set[E]) SymmetricDifference(other *Set[E]) *Set[E] {
 }
 
 func (s *Set[E]) SubsetOf(other *Set[E]) bool {
-	for item := range s.items {
+	for item := range s.Iter() {
 		if !other.Has(item) {
 			return false
 		}
@@ -101,6 +101,16 @@ func (s *Set[E]) SubsetOf(other *Set[E]) bool {
 
 func (s *Set[E]) SuperSetOf(other *Set[E]) bool {
 	return other.SubsetOf(s)
+}
+
+func (s *Set[E]) Iter() iter.Seq[E] {
+	return func(yield func(E) bool) {
+		for e := range s.items {
+			if !yield(e) {
+				return
+			}
+		}
+	}
 }
 
 func New[E comparable](rangable iter.Seq[E]) *Set[E] {

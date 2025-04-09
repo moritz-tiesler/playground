6a4324c8f71f0c431bb42e27adde52caef559f33
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-04-10 00:27:07 +0200

    differenece, equals, subset, superset

--- set/set.go
+++ set/set.go
@@ -3,6 +3,7 @@ package set
 import (
 	"iter"
 	"maps"
+	"reflect"
 	"slices"
 )
 
@@ -46,6 +47,21 @@ func (s *Set[E]) Intersection(other *Set[E]) *Set[E] {
 	return intersected
 }
 
+func (s *Set[E]) Difference(other *Set[E]) *Set[E] {
+	empty := slices.Values([]E{})
+	difference := New(empty)
+	for item := range s.items {
+		if !other.Has(item) {
+			difference.Put(item)
+		}
+	}
+	return difference
+}
+
+func (s *Set[E]) Equals(other *Set[E]) bool {
+	return reflect.DeepEqual(s.items, other.items)
+}
+
 func (s *Set[E]) Union(other *Set[E]) *Set[E] {
 	unioned := New(s.Items())
 	for item := range other.items {
@@ -69,6 +85,19 @@ func (s *Set[E]) SymmetricDifference(other *Set[E]) *Set[E] {
 		}
 	}
 	return diff1.Union(diff2)
+}
+
+func (s *Set[E]) SubsetOf(other *Set[E]) bool {
+	for item := range s.items {
+		if !other.Has(item) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *Set[E]) SuperSetOf(other *Set[E]) bool {
+	return other.SubsetOf(s)
 }
 
 func New[E comparable](rangable iter.Seq[E]) *Set[E] {

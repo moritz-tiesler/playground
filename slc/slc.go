6b310cb078d901d28ef74a76db0665b3ac0577a3
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-07-09 21:50:38 +0200

    ChunkFunc

--- slc/slc.go
+++ slc/slc.go
@@ -2,6 +2,7 @@ package slc
 
 import (
 	"cmp"
+	"iter"
 	"slices"
 )
 
@@ -24,4 +25,40 @@ func ToSortedClone[T cmp.Ordered](ss []T) []T {
 	copy(o, ss)
 	slices.Sort(o)
 	return o
+}
+
+// ChunkFunc returns an iterator over consecutive sub-slices.
+// When the funcion passed to ChunkFunc returns true for a given a and b,
+// a cut will be made be made after the index of a and a chunk will be returned.
+func ChunkFunk[T any](s []T, doCut func(a, b T) bool) iter.Seq[[]T] {
+	chunk := []T{}
+	left, right := 0, 1
+	return func(yield func([]T) bool) {
+		if len(s) < 2 {
+			yield(s)
+			return
+		}
+		for {
+			if left >= len(s) {
+				return
+			}
+			if right >= len(s) {
+				end := min(right, len(s))
+				chunk = s[left:end:end]
+				yield(chunk)
+				return
+			}
+			if !doCut(s[right-1], s[right]) {
+				right++
+				continue
+			} else {
+				chunk = s[left:right:right]
+				if !yield(chunk) {
+					return
+				}
+				left = right
+				right++
+			}
+		}
+	}
 }
--- slc/slc_test.go
+++ slc/slc_test.go
@@ -2,6 +2,7 @@ package slc
 
 import (
 	"math/rand/v2"
+	"reflect"
 	"slices"
 	"testing"
 )
@@ -79,6 +80,115 @@ func BenchmarkToSortedSeq(b *testing.B) {
 		s := slices.Sorted(slices.Values(u))
 		if s[len(s)-1] != max {
 			b.Errorf("sorting wrong")
+		}
+	}
+}
+
+func TestChunkFunk(t *testing.T) {
+	f := func(a, b int) bool {
+		if b == a*2 {
+			return true
+		}
+		return false
+	}
+
+	{
+		seq := ChunkFunk([]int{1, 2, 3, 6, 7, 8}, f)
+
+		expected := [][]int{
+			{1},
+			{2, 3},
+			{6, 7, 8},
+		}
+		result := slices.Collect(seq)
+		if !reflect.DeepEqual(
+			result,
+			expected,
+		) {
+			t.Fatalf("expected=%v, got=%v", expected, result)
+		}
+	}
+	{
+		seq := ChunkFunk([]int{1}, f)
+
+		expected := [][]int{
+			{1},
+		}
+		result := slices.Collect(seq)
+		if !reflect.DeepEqual(
+			result,
+			expected,
+		) {
+			t.Fatalf("expected=%v, got=%v", expected, result)
+		}
+	}
+	{
+		seq := ChunkFunk([]int{}, f)
+
+		expected := [][]int{
+			{},
+		}
+		result := slices.Collect(seq)
+		if !reflect.DeepEqual(
+			result,
+			expected,
+		) {
+			t.Fatalf("expected=%v, got=%v", expected, result)
+		}
+	}
+	{
+		var s []int
+		seq := ChunkFunk(s, f)
+
+		var empty []int
+		expected := [][]int{
+			empty,
+		}
+		result := slices.Collect(seq)
+		if !reflect.DeepEqual(
+			result,
+			expected,
+		) {
+			t.Fatalf("expected=%v, got=%v", expected, result)
+		}
+	}
+	{
+		f := func(a, b int) bool {
+			return false
+		}
+
+		seq := ChunkFunk([]int{1, 2, 3, 6, 7, 8}, f)
+		expected := [][]int{
+			{1, 2, 3, 6, 7, 8},
+		}
+		result := slices.Collect(seq)
+		if !reflect.DeepEqual(
+			result,
+			expected,
+		) {
+			t.Fatalf("expected=%v, got=%v", expected, result)
+		}
+	}
+	{
+		f := func(a, b int) bool {
+			return true
+		}
+
+		seq := ChunkFunk([]int{1, 2, 3, 6, 7, 8}, f)
+		expected := [][]int{
+			{1},
+			{2},
+			{3},
+			{6},
+			{7},
+			{8},
+		}
+		result := slices.Collect(seq)
+		if !reflect.DeepEqual(
+			result,
+			expected,
+		) {
+			t.Fatalf("expected=%v, got=%v", expected, result)
 		}
 	}
 }
